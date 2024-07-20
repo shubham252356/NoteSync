a8c556d4e35b552f7764dccddeb6df3d5a14b0f9
shubham chavan <shubhamc252356@gmail.com>
2024-07-20 18:04:52 +0530

    initial commit

---
+++ src/main/java/com/application/notesync/NotesyncApplication.java
@@ -0,0 +1,13 @@
+package com.application.notesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesyncApplication.class, args);
+	}
+
+}
